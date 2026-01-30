3998e139d2ed3bbe336ee76c2b8e883957ca791e
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2026-01-30 22:04:05 +0530

    setup clerk and neon database

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -2,5 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLIENT_URL: process.env.CLIENT_URL
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express"
+import cors from "cors"
 import { ENV } from "./config/env"
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
+app.use(cors({origin: ENV.CLIENT_URL}));
+app.use(clerkMiddleware())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
-    res.json({success: true});
+    res.json({
+        message: "Welcome to OnePlace",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments"
+        }
+    });
 })
 
 app.listen(ENV.PORT, () => {
