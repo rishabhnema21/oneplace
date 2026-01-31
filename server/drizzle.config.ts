aa62739aa32c59a1979dbfdfdac9ac651826cde2
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2026-01-31 07:22:40 +0530

    setup drizzle orm with postgres config and db schema

---
+++ server/drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit"
+import { ENV } from "./src/config/env"
+
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {url: ENV.DB_URL!}
+}) 
